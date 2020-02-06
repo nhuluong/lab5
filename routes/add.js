187ab57f1fba3b9918aae6a725af38be91c9c745
Nhu Luong <nhuluong@Nhus-MacBook-Pro.local>
2020-02-06 13:01:55 -0800

    guess im done

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,18 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+
+	var qNAme = request.query.name;
+	var qDesc = request.query.description;
+
+	var newFriend = {
+		"name": qName,
+		"description": qDesc,
+		"imageURL": "https://lorempixel.com/400/400/profile"
+	}
+
+	console.log(newFriend);
+	data.friends.push(newFriend);
+
+	response.render('index', data);
  }
